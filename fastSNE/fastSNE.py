80af722fc772389a8254b5727704e5fe78fe1359
gneeeeeh <17829183+PierreLambert3@users.noreply.github.com>
2024-09-01 19:16:46 +0200

    qs

--- fastSNE/fastSNE.py
+++ fastSNE/fastSNE.py
@@ -1,49 +1,133 @@
-# a class that will contain the data and the results of the fastSNE algorithm
 import numpy as np
 import multiprocessing
 
-import pycuda.autoinit
+
+# import & init pycuda
 import pycuda.driver as cuda
-import pycuda.gpuarray as gpuarray
+import pycuda.autoinit
+
+__MAX_PP__ = 80
+__MAX_K__  = __MAX_PP__ * 3
 
 class fastSNE:
     def __init__(self, with_GUI, n_components=2, random_state=None):
-        # model state
         self.with_GUI     = with_GUI
         self.is_fitted    = False
         self.random_state = random_state
         if self.random_state is not None and not self.random_state > 0:
             raise Exception("fastSNE: random_state must be a strictly positive integer")
-        # dataset info 	8.9
+        self.N        = None
+        self.Mhd      = None
+        self.cpu_Xhd  = None
+        self.Mld      = np.uint32(n_components)
+        self.kern_alpha   = np.float32(1.0)
+        self.perplexity   = np.float32(5.0)
+        self.dist_metric  = 0
+
+        self.shared_buffer = None # for GUI
+
+    
+    def fit(self, N, M, X, Y=None):
+        self.N   = N
+        self.M   = M
+        self.cpu_Xhd  = X
+        
+        if self.with_GUI:
+            self.fit_with_gui(Y)
+        else:
+            self.fit_without_gui()
+        self.is_fitted = True
+
+    def transform(self):
+        if not self.is_fitted:
+            raise Exception("fastSNE: transform() called before fit(), or fit failed crashingly")
+        # return self.cpu_Xld
+        return None
+
+    def one_iteration(self):
+        return
+
+    def fit_with_gui(self, Y):
+        # 0. configure the process launch mode 
+        multiprocessing.set_start_method('spawn')
+
+        # 1.   Launching the process responsible for the GUI
+        from fastSNE.fastSNE_gui import gui_worker, Shared_CPU_variables
+        # 1.1  Inter-process communication for CUDA structures
+        buffer_n_bytes = self.N * __MAX_K__ * 4 # x4 For float32
+        self.shared_buffer_A = cuda.mem_alloc(buffer_n_bytes)
+        self.shared_buffer_B = cuda.mem_alloc(buffer_n_bytes)
+        ipc_handle_Xld_A = cuda.mem_get_ipc_handle(self.shared_buffer_A)
+        ipc_handle_Xld_B = cuda.mem_get_ipc_handle(self.shared_buffer_B)
+        # 1.2  Shared variable on CPU
+        shared_CPU_variables = Shared_CPU_variables(self.kern_alpha, self.perplexity, self.dist_metric)
+        print("Serialize Shared Variable: If the Shared_variable class instance is not directly serializable (e.g., for use with multiprocessing.Queue or Pipe), you might need to manually serialize it or ensure it only contains serializable elements.")
+        # 1.3  Launching the GUI process proper
+        process_gui = multiprocessing.Process(target=gui_worker, args=([ipc_handle_Xld_A, ipc_handle_Xld_B], Y, shared_CPU_variables, self.N, self.Mld))
+        process_gui.start()
+
+        # 2.   Optimise until the GUI is closed
+        gui_was_closed = False
+        while not gui_was_closed:
+            self.one_iteration()
+
+            with shared_CPU_variables.points_have_moved.get_lock():
+                shared_CPU_variables.points_have_moved.value = True
+
+            with shared_CPU_variables.gui_closed.get_lock():
+                gui_was_closed = shared_CPU_variables.gui_closed.value
+        process_gui.join()
+
+
+        self.free_all_GPU_memory()
+        print("fastSNE: GUI process has terminated")
+        return
+    
+    def fit_without_gui(self):
+        for i in range(100):
+            self.one_iteration()
+        self.cpu_Xld = self.cuda_Xld_true_A.get()
+        self.free_all_GPU_memory()
+
+    def free_all_GPU_memory(self):
+        # cuda_context.pop()
+        """ self.cuda_Xld_true_A.gpudata.free()
+        self.cuda_Xld_true_B.gpudata.free()
+        self.cuda_Xld_nest.gpudata.free()
+        self.cuda_Xld_mmtm.gpudata.free()
+        self.cuda_Xhd.gpudata.free() """
+        return
+
+""" 
+class fastSNE:
+    def __init__(self, with_GUI, n_components=2, random_state=None):
+        self.with_GUI     = with_GUI
+        self.is_fitted    = False
+        self.random_state = random_state
+        if self.random_state is not None and not self.random_state > 0:
+            raise Exception("fastSNE: random_state must be a strictly positive integer")
         self.N   = None
         self.M   = None
         self.cpu_Xhd  = None
         self.cuda_Xhd = None
-        # model results
         self.cpu_Xld  = None
-        # optimiser
         self.cuda_Xld_true_A = None
         self.cuda_Xld_true_B = None
         self.cuda_Xld_nest   = None
         self.cuda_Xld_mmtm   = None
         print("For rendering: use directly the data that is on GPU (else GPU->CPU then CPU->GPU: wasteful)")
-        # algorithm parameters
         self.n_components = np.uint32(n_components)
         self.kern_alpha   = np.float32(1.0)
         self.perplexity   = np.float32(5.0)
-        self.dist_metric  = 0 # 0: euclidean, 1: cosine, 2: manhattan
+        self.dist_metric  = 0
 
     def fit(self, N, M, X, Y=None):
-        # --------  on CPU  --------
         self.N   = N
         self.M   = M
         self.cpu_Xhd  = X
-        # --------  on GPU  --------
-        # cuda streams 
         self.stream_neigh_HD = pycuda.driver.Stream()
         self.stream_neigh_LD = pycuda.driver.Stream()
         self.stream_grads    = pycuda.driver.Stream()
-        # data
         self.cuda_Xhd        = gpuarray.to_gpu_async(self.cpu_Xhd)
         self.cpu_Xld         = (np.random.uniform(size=(N, self.n_components)).astype(np.float32) - 0.5) * 2.0
         self.cuda_Xld_true_A = gpuarray.to_gpu(self.cpu_Xld)
@@ -64,48 +148,33 @@ class fastSNE:
         return self.cpu_Xld
 
     def one_iteration(self):
-        """ W = np.random.randn(self.M, self.n_components)
-        self.Xld_true_A = np.dot(self.Xhd, W) """
-        pass
+        return;
 
-
-    ok, passer en single process et tout faire en async calls
-
-    def fit_with_gui(self, Y): # Y is for visualisation only, it doesn't intervene in the algorithm
+    def fit_with_gui(self, Y):
         multiprocessing.set_start_method('spawn')
-        # initialise the shared variables between the GUI and the worker
         from fastSNE.fastSNE_gui import Shared_variables
         shared_variables = Shared_variables(self.cuda_Xld_true_A, self.cuda_Xld_true_B, self.kern_alpha, self.perplexity, self.dist_metric)
-        # start the GUI
         from fastSNE.fastSNE_gui import gui_worker
         cuda_Y = gpuarray.to_gpu(Y)
         process_gui = multiprocessing.Process(target=gui_worker, args=(cuda_Y, shared_variables, self.N, self.M, self.n_components))
         process_gui.start()
-        # run till gui is closed
         gui_was_closed = False
         while not gui_was_closed:
-            # one iteration of the algorithm
             self.one_iteration()
 
-            """ # tell the GUI that points have moved
             with shared_variables.shared_points_have_moved.get_lock():
-                shared_variables.shared_points_have_moved.value = True """
+                shared_variables.shared_points_have_moved.value = True
 
-            # check if the GUI was closed
             with shared_variables.shared_gui_closed.get_lock():
                 gui_was_closed = shared_variables.shared_gui_closed.value
         process_gui.join()
-        # synchronuous copy (GPU->CPU)
         self.cpu_Xld = self.cuda_Xld_true_A.get()
-        # free all the GPU memory
         self.free_all_GPU_memory()
     
     def fit_without_gui(self):
         for i in range(100):
             self.one_iteration()
-        # synchronuous copy (GPU->CPU)
         self.cpu_Xld = self.cuda_Xld_true_A.get()
-        # free all the GPU memory
         self.free_all_GPU_memory()
 
     def free_all_GPU_memory(self):
@@ -117,3 +186,4 @@ class fastSNE:
 
 
         
+ """
--- fastSNE/fastSNE_gui.py
+++ fastSNE/fastSNE_gui.py
@@ -1,15 +1,14 @@
-#include the classics
+# import the classics
 import numpy as np
 import multiprocessing
 
-#include modernGL (renderer) and pyglet (windowerer)
+# import modernGL (renderer) and pyglet (windowerer)
 import moderngl as mgl
 import pyglet
 
-import pycuda.gpuarray as gpuarray
+__TARGET_FPS__ = 20.0
 
 def gen_K_random_colours(K):
-    # K means in a 3D space
     n = K * 200
     dataset = np.random.uniform(0, 1, (n, 3))
     from sklearn.cluster import KMeans
@@ -41,36 +40,36 @@ def determine_Y_colour(Y):
             cpu_Y_colours[i, 0] = np.uint8(y_normed*red1 + (1.0-y_normed)*red0)
             cpu_Y_colours[i, 1] = np.uint8(y_normed*green1 + (1.0-y_normed)*green0)
             cpu_Y_colours[i, 2] = np.uint8(y_normed*blue1 + (1.0-y_normed)*blue0)
+    from pycuda import gpuarray
     cuda_Y_colours = gpuarray.to_gpu(cpu_Y_colours)
     return cuda_Y_colours
 
-class Shared_variables:
-    def __init__(self, cuda_Xld_A, cuda_Xld_B, init_kern_alpha, init_perplexity, init_dist_metric):
+class Shared_CPU_variables:
+    def __init__(self, init_kern_alpha, init_perplexity, init_dist_metric):
         # mutliprocessing Value types
-        self.shared_kern_alpha  = multiprocessing.Value('f', init_kern_alpha)
-        self.shared_perplexity  = multiprocessing.Value('f', init_perplexity)
-        self.shared_dist_metric = multiprocessing.Value('i', init_dist_metric)
-        self.shared_gui_closed  = multiprocessing.Value('b', False)
-        self.shared_isPhaseA    = multiprocessing.Value('b', True)
-        self.shared_iterDone1   = multiprocessing.Value('i', 0)
-        self.shared_iterDone1   = multiprocessing.Value('i', 0)
-        self.shared_points_have_moved = multiprocessing.Value('b', True)
-        # on GPU
-        self.cuda_Xld_A = cuda_Xld_A
-        self.cuda_Xld_B = cuda_Xld_B
+        self.kern_alpha  = multiprocessing.Value('f', init_kern_alpha)
+        self.perplexity  = multiprocessing.Value('f', init_perplexity)
+        self.dist_metric = multiprocessing.Value('i', init_dist_metric)
+        self.gui_closed  = multiprocessing.Value('b', False)
+        self.isPhaseA    = multiprocessing.Value('b', True)
+        self.iterDone1   = multiprocessing.Value('i', 0)
+        self.iterDone2   = multiprocessing.Value('i', 0)
+        self.points_have_moved = multiprocessing.Value('b', True)
     
     def get_reading_Xld(self):
-        return self.cuda_Xld_A if self.shared_isPhaseA.value else self.cuda_Xld_B
+        return self.cuda_Xld_A if self.isPhaseA.value else self.cuda_Xld_B
     
     def get_writing_Xld(self):
-        return self.cuda_Xld_B if self.shared_isPhaseA.value else self.cuda_Xld_A
+        return self.cuda_Xld_B if self.isPhaseA.value else self.cuda_Xld_A
 
 class ModernGLWindow(pyglet.window.Window):
-    def __init__(self, Y, shared_variables, N, Mld, **kwargs):
+    def __init__(self, cuda_Xld_A, cuda_Xld_B, Y, shared_CPU_variables, N, Mld, **kwargs):
         super().__init__(**kwargs)
-        self.shared_variables = shared_variables
+        self.shared_CPU_variables = shared_CPU_variables
+        self.cuda_Xld_A = cuda_Xld_A
+        self.cuda_Xld_B = cuda_Xld_B
         self.Y = Y
-        self.Y_colours = determine_Y_colour(Y) # size (N, 3), colour for each observation (removes an "if" in the render function)
+        self.cuda_Y_colours = determine_Y_colour(Y) # size (N, 3), colour for each observation (removes an "if" in the render function)
         self.ctx = mgl.create_context()
         self.prog = self.ctx.program(
             vertex_shader='''
@@ -88,11 +87,10 @@ class ModernGLWindow(pyglet.window.Window):
                 }
             ''',
         )
-        self.vbo = self.ctx.buffer(reserve=N * 2 * 4)  # Reserve buffer size for 2D points, adjust as needed
+        self.vbo = self.ctx.buffer(reserve=N * 2 * 4)
         self.vao = self.ctx.simple_vertex_array(self.prog, self.vbo, 'in_vert')
 
     def on_draw(self):
-        # print("Ensure you bind the necessary resources (like VAO) before drawing and unbind them afterward.")
         self.clear()
         self.vao.render(mgl.POINTS)
     
@@ -102,34 +100,28 @@ class ModernGLWindow(pyglet.window.Window):
         self.ctx.release()
         super().on_close()
     
-    # called every frame
     def update(self, dt):
-        # were the points moved?
         point_have_moved = False
-        with self.shared_variables.shared_points_have_moved.get_lock():
-            point_have_moved = self.shared_variables.shared_points_have_moved.value
-            self.shared_variables.shared_points_have_moved.value = False
-        # if yes, update the data in the VBO
+        with self.shared_CPU_variables.points_have_moved.get_lock():
+            point_have_moved = self.shared_CPU_variables.points_have_moved.value
+            self.shared_CPU_variables.points_have_moved.value = False
         if point_have_moved:
             print("Update the data in the VBO every frame")
         return
 
     def on_mouse_press(self, x, y, button, modifiers):
-        # differentiate button types
         if button == pyglet.window.mouse.LEFT:
             print("Left mouse button pressed")
         elif button == pyglet.window.mouse.RIGHT:
             print("Right mouse button pressed")
     
     def on_mouse_release(self, x, y, button, modifiers):
-        # differentiate button types
         if button == pyglet.window.mouse.LEFT:
             print("Left mouse button released")
         elif button == pyglet.window.mouse.RIGHT:
             print("Right mouse button released")
     
     def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
-        # differentiate button types
         if buttons & pyglet.window.mouse.LEFT:
             print("Left mouse button dragged")
         elif buttons & pyglet.window.mouse.RIGHT:
@@ -146,21 +138,30 @@ class ModernGLWindow(pyglet.window.Window):
             self.close()
 
 class FastSNE_gui:
-    def __init__(self, Y, shared_variables, N: np.uint32, Mld: np.uint32, window_w: np.uint32=640, window_h: np.uint32=480):
-        # window 
-        self.window = ModernGLWindow(Y, shared_variables, N, Mld, width=window_w, height=window_h, caption='fastSNE')
-        # re-render period
-        target_fps = 20.0
-        pyglet.clock.schedule_interval(self.window.update, 1.0/target_fps)
-        # Start the pyglet event loop
-        pyglet.app.run()
+    def __init__(self, cuda_Xld_A, cuda_Xld_B, Y, shared_CPU_variables, N: np.uint32, Mld: np.uint32, window_w: np.uint32=640, window_h: np.uint32=480):
+        self.window = ModernGLWindow(cuda_Xld_A, cuda_Xld_B, Y, shared_CPU_variables, N, Mld, width=window_w, height=window_h, caption='fastSNE')
+        pyglet.clock.schedule_interval(self.window.update, 1.0/__TARGET_FPS__)
+        pyglet.app.run() # blocks until the window is closed, everything is event-driven from there on
 
-def gui_worker(cuda_Y, shared_variables, N, Mld, n_components):
-    # create and launch the GUI (GUI = window + renderer + event loop)
-    gui = FastSNE_gui(Y, shared_variables, N, Mld, window_w=800, window_h=600)
+def gui_worker(ipc_handles_list, cpu_Y, shared_CPU_variables, N, Mld):
+    # import & init pycuda
+    import pycuda.driver as cuda
+    import pycuda.autoinit
+    cuda.init()
+    cuda_context = cuda.Device(0).make_context()
 
-    # notify the main process that the GUI was closed
-    with shared_variables.shared_gui_closed.get_lock():
+    # ipc for Xld_A and Xld_B
+    cuda_Xld_A = ipc_handles_list[0]
+    cuda_Xld_B = ipc_handles_list[1]
+    gui = FastSNE_gui(cuda_Xld_A, cuda_Xld_B, cpu_Y, shared_CPU_variables, N, Mld, window_w=800, window_h=600)
+
+    # notify the main process that the GUI has been closed
+    with shared_CPU_variables.gui_closed.get_lock():
         pyglet.app.exit()
-        shared_variables.shared_gui_closed.value = True
+        shared_CPU_variables.gui_closed.value = True
+    
+    # release the pycuda context
+    cuda_context.pop()
     return
+
+
